e77c5675fbba51985b0f7205018926f68b05fd4d
Akash Nerella <100755887+akashnerella@users.noreply.github.com>
2022-04-01 16:21:03 +0530

    Update index.js

--- Akash_PGB2022/Inevitable_coders/Frontend/myapp/src/Components/Home/index.js
+++ Akash_PGB2022/Inevitable_coders/Frontend/myapp/src/Components/Home/index.js
@@ -1,22 +1,27 @@
 import "./index.css"
-import { useHistory } from "react-router-dom"
+import { useNavigate} from "react-router-dom"
 
 const Home=()=>{
-  const History=useHistory()
-  const func=()=>{
-  History.push("/Java");}
-
-  const func1=()=>{
-  History.push("/Python");}
-
-  const func2=()=>{
-    History.push("/JavaScript");
+  
+  const History=useNavigate()
+  
+  const func=()=>{  
+    History("/Java");
   }
+
+  const func1=()=>{   
+    History("/Python");
+  }
+
+  const func2=()=>{ 
+    History("/JavaScript");
+  }
+  
   return(
+    
         <div className="bg-container">
           <h1 className="home_head" align="center">Welcome,Test your programming abilities</h1>
-            <div className="home_container">
-                
+            <div className="home_container">  
                 <img src="/images/javaimage.png" alt="javaimg" className="imgE1" onClick={func} path="/Java" />
                 <img src="/images/py.png" alt="pythomimg" className="imgE" onClick={func1} path="/Python"/>
                 <img src="/images/javaS.jpg" alt="javasriptimg" className="imgE" onClick={func2} path="/JavaScript"/>
