2eb77887ff0971334922e049ced96cbeddcc0751
Akash Nerella <100755887+akashnerella@users.noreply.github.com>
2022-04-01 16:20:10 +0530

    Update App.js

--- Akash_PGB2022/Inevitable_coders/Frontend/myapp/src/App.js
+++ Akash_PGB2022/Inevitable_coders/Frontend/myapp/src/App.js
@@ -3,17 +3,22 @@ import Home from "./Components/Home"
 import Java from "./Components/Java"
 import Python from "./Components/Python"
 import JavaScript from "./Components/JavaScript"
-import {BrowserRouter,Switch,Route} from "react-router-dom"
+import NotFound from './Components/NotFound';
+import {BrowserRouter,Routes,Route} from "react-router-dom"
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Switch>
-          <Route path="/Java" component={Java}/>
-          <Route path="/Python" component={Python}/>
-          <Route path="/JavaScript" component={JavaScript}/>
-          <Route exact path="/" component={Home}/>
-        </Switch>
+        <Routes>
+          <Route exact path="/" element={<Home/>}/>
+          <Route path="/Java" element={<Java start="0" stop="10"/>}/>
+          <Route path="/Java2" element={<Java start="10" stop="20"/>}/>
+          <Route path="/Python" element={<Python start="0" stop="10"/>}/>
+          <Route path="/Python2" element={<Python  start="10" stop="20"/>}/>
+          <Route path="/JavaScript" element={<JavaScript start="0" stop="10"/>}/>
+          <Route path="/JavaScript2" element={<JavaScript start="10" stop="20"/>}/>          
+          <Route path="*" element={<NotFound/>} />
+        </Routes>
       </BrowserRouter>
     </>
   );
